a37d2518ddcace03219db0abe0d306a8b294fd09
Anderson Lemos <alemos@dmorj.com.br>
2015-02-27 11:59:27 -0300

    update filter

--- contatooh/public/js/controllers/ContatosController.js
+++ contatooh/public/js/controllers/ContatosController.js
@@ -3,5 +3,14 @@ angular.module('contatooh').controller('ContatosController',
              $scope.total = 0;
              $scope.incrementa = function () {
                  $scope.total++;
-            };
+             };
+             $scope.contatos = [
+                                    {_id:1,nome:'Contato 1',email:'contato1@contatooh.com'},
+                                    {_id:2,nome:'Contato 2',email:'contato2@contatooh.com'},
+                                    {_id:3,nome:'Contato 3',email:'contato3@contatooh.com'}
+                                 ];
+             $scope.filtro='';
+
+             
+             
 });
