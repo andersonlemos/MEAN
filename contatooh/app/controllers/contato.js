7bc04933c64cc7ad7e09b66966b8d40f60c1348e
anderson <anderson@localhost.localdomain>
2015-04-01 23:27:02 -0300

    contatosController atualizado

--- contatooh/app/controllers/contato.js
+++ contatooh/app/controllers/contato.js
@@ -1,6 +1,7 @@
 var contatos = [{_id:1,nome:'Contato 1',email:'contato1@contatooh.com'},
                 {_id:2,nome:'Contato 2',email:'contato2@contatooh.com'},
-                {_id:3,nome:'Contato 3',email:'contato3@contatooh.com'}
+                {_id:3,nome:'Contato 3',email:'contato3@contatooh.com'},
+                {_id:4,nome:'Contato 4',email:'contato4@contatooh.com'}
                ];
 
 module.exports = function (){
--- contatooh/public/js/controllers/ContatosController.js
+++ contatooh/public/js/controllers/ContatosController.js
@@ -1,16 +1,25 @@
 angular.module('contatooh').controller('ContatosController',
-    function ($scope) {
+    function ($scope,$http) {
+             
+             $scope.contatos=[];
+             
              $scope.total = 0;
+         
+             $scope.filtro='';
+
              $scope.incrementa = function () {
                  $scope.total++;
              };
-             $scope.contatos = [
-                                    {_id:1,nome:'Contato 1',email:'contato1@contatooh.com'},
-                                    {_id:2,nome:'Contato 2',email:'contato2@contatooh.com'},
-                                    {_id:3,nome:'Contato 3',email:'contato3@contatooh.com'}
-                                 ];
-             $scope.filtro='';
-
+             
+             $http.get('/contatos')
+             .success(function(data){
+                 $scope.contatos=data;
+             })
+             .error(function(statusError){
+                 console.log("Não foi possível obter a lista de contatos");
+                 console.log(statusError);
+             });
+         
              
              
 });
