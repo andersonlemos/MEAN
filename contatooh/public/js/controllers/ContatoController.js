35b9c476781864d2fdea9dc9a98dc7d1643eeb15
AndersonLemos <alemos@openmailbox.org>
2015-04-05 22:17:18 -0300

    update

--- contatooh/public/js/controllers/ContatoController.js
+++ contatooh/public/js/controllers/ContatoController.js
@@ -1,4 +1,20 @@
 angular.module('contatooh').controller('ContatoController',
-    function ($scope,$routeParams) {
-           console.log($routeParams.contatoId);        
-});
+    function($scope, $resource, $routeParams) {
+        var Contato = $resource('/contatos/:id');
+        });
+            if($routeParams.contatoId) {
+                
+            Contato.get({id: $routeParams.contatoId},
+       
+        function(contato) {
+            $scope.contato = contato;
+        },
+        function(erro) {
+            $scope.mensagem = {
+                        texto: 'Contato não existe. Novo contato.'
+                    };
+            }
+        );
+    } else {
+        $scope.contato = {};
+}   
